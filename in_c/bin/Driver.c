4027e8dfa7b84cb12abfa5ed75b4b437ee2724bd
Alex Vesey <no@email.com>
2020-07-16 22:21:45 -0400

    remove all warnings, add output

--- in_c/bin/Driver.c
+++ in_c/bin/Driver.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <flmoon.h>
 
 int main()
 {
   int exit_code = 1;
 
+  printf("////////////////////////////////////\n");
   printf("The Driver for NumericalRecepiesInC\n");
+  printf("////////////////////////////////////\n\n");
 
   int n = 1;
   int nph = 0;
-  long *jd = 0;
-  float *frac = 0;
+  long *jd = malloc(sizeof(long));
+  float *frac = malloc(sizeof(float));
 
   flmoon(n, nph, jd, frac);
 
-  fprintf("n=%i , nph=%i, jd=%d, frac=%d",n,nph,jd,frac);
+  printf("flmoon test: ");
+  printf("n=%i , nph=%i, jd=%ld, frac=%lf\n",n,nph,*jd,(double)*frac);
 
   return 0;
 }
