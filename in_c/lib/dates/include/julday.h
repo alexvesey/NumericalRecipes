9ac6e99fe6c76212afa84690a0cc1eec1477e827
Alex Vesey <no@email.com>
2020-07-22 20:03:15 -0400

    Implemented BadLuck; implemented caldat in c and tested

--- in_c/lib/dates/include/julday.h
+++ in_c/lib/dates/include/julday.h
@@ -3,7 +3,10 @@
 
 #include <math.h>
 #define IGREG (15+31L*(10+12L*1582))
+#define CALIGREG 2299161
 
 long julday(int mm, int id, int iyyy);
+
+void caldat(long julian, int *mm, int *id, int *iyyy);
 
 #endif
